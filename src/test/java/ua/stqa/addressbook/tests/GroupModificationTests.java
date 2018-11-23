8bb842f347a78edcd56c4b592caaa13dee2d18d1
Vitaliy Falaleyev <vitaliy.lviv88@gmail.com>
2018-11-23 19:17:28 +0200

    browser options

--- src/test/java/ua/stqa/addressbook/tests/GroupModificationTests.java
+++ src/test/java/ua/stqa/addressbook/tests/GroupModificationTests.java
@@ -11,6 +11,8 @@ public class GroupModificationTests extends TestBase {
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
 
 
     }
