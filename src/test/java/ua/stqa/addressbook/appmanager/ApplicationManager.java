daf83ba3cb7beead9bbf6fdaae9c3805af9d6d62
Vitaliy Falaleyev <vitaliy.lviv88@gmail.com>
2018-11-27 18:31:53 +0200

    update

--- src/test/java/ua/stqa/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ua/stqa/addressbook/appmanager/ApplicationManager.java
@@ -1,13 +1,11 @@
 package ua.stqa.addressbook.appmanager;
 
-
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
-
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -23,11 +21,11 @@ public class ApplicationManager {
     }
 
     public void init() {
-          if (browser == BrowserType.FIREFOX) {
+          if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
-        } else if (browser == BrowserType.CHROME) {
+        } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser == BrowserType.IE) {
+        } else if (browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
         }
 
