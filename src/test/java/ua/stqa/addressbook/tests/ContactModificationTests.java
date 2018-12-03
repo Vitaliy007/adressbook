4009f656b195ab025822e4dbd62d523518ea6b01
Vitaliy Falaleyev <vitaliy.lviv88@gmail.com>
2018-12-03 18:58:44 +0200

    assert lists

--- src/test/java/ua/stqa/addressbook/tests/ContactModificationTests.java
+++ src/test/java/ua/stqa/addressbook/tests/ContactModificationTests.java
@@ -9,14 +9,11 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactCreation() {
         app.getNavigationHelper().gotoHomePage();
-        int before = app.getGroupHelper().getGroupCount();
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData("test_name", "test_surname", null), false);
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToHomePage();
         int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
-
     }
 
 }
--- src/test/java/ua/stqa/addressbook/tests/GroupModificationTests.java
+++ src/test/java/ua/stqa/addressbook/tests/GroupModificationTests.java
@@ -1,7 +1,10 @@
 package ua.stqa.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.stqa.addressbook.model.GroupData;
+
+import java.util.List;
 
 public class GroupModificationTests extends TestBase {
 
@@ -11,12 +14,14 @@ public class GroupModificationTests extends TestBase {
         if(! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
-        app.getGroupHelper().selectGroup();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
 
     }
 }
