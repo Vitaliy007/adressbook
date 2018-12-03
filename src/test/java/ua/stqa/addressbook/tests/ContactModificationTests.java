7639f45c28d8c91fe40a5fc2c06e5b887e3c3113
Vitaliy Falaleyev <vitaliy.lviv88@gmail.com>
2018-12-03 18:30:27 +0200

    group assertions

--- src/test/java/ua/stqa/addressbook/tests/ContactModificationTests.java
+++ src/test/java/ua/stqa/addressbook/tests/ContactModificationTests.java
@@ -1,5 +1,6 @@
 package ua.stqa.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.stqa.addressbook.model.ContactData;
 
@@ -8,11 +9,13 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactCreation() {
         app.getNavigationHelper().gotoHomePage();
+        int before = app.getGroupHelper().getGroupCount();
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData("test_name", "test_surname", null), false);
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToHomePage();
-
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before);
 
     }
 
