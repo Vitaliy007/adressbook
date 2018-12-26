c1c5d74242e62157cd5af4670944925d14d422bb
Vitaliy Falaleyev <vitaliy.lviv88@gmail.com>
2018-12-26 18:55:46 +0200

    properties

--- src/test/java/ua/stqa/addressbook/generators/GroupDataGenerator.java
+++ src/test/java/ua/stqa/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package ua.stqa.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import ua.stqa.addressbook.model.GroupData;
 
@@ -42,27 +44,38 @@ public class GroupDataGenerator {
             saveAsCsv(groups, new File(file));
         }   else if (format.equals("xml")) {
             saveAsXml(groups, new File(file));
-        } else {
+        } else if (format.equals("json")){
+            saveAsJson(groups, new File(file));
+        }
+        else {
             System.out.println("unrecognized format" + format);
         }
 
+    }
+
+    private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(groups);
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(json);
+        }
     }
 
     private void saveAsXml(List<GroupData> groups, File file) throws IOException {
         XStream xstream = new XStream();
         xstream.processAnnotations(GroupData.class);
         String xml = xstream.toXML(groups);
-        Writer writer = new FileWriter(file);
-        writer.write(xml);
-        writer.close();
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
     }
 
     private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
-            Writer writer = new FileWriter(file);
-            for (GroupData group : groups) {
-                writer.write(String.format("%s;%s;%s\n", group.getName(),group.getHeader(),group.getFooter()));
+            try (Writer writer = new FileWriter(file)) {
+                for (GroupData group : groups) {
+                    writer.write(String.format("%s;%s;%s\n", group.getName(),group.getHeader(),group.getFooter()));
+                }
             }
-            writer.close();
         }
 
 
--- src/test/java/ua/stqa/addressbook/tests/GroupCreationTests.java
+++ src/test/java/ua/stqa/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package ua.stqa.addressbook.tests;
 
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -23,32 +25,37 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() throws IOException {
-        List<Object[]> list = new ArrayList<Object[]>();
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
-        String xml = "";
-        String line = reader.readLine();
-        while (null != line) {
-            xml += line;
-            line = reader.readLine();
+    public Iterator<Object[]> validGroupsFromXML() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))) {
+            String xml = "";
+            String line = reader.readLine();
+            while (null != line) {
+                xml += line;
+                line = reader.readLine();
+            }
+            XStream xstream = new XStream();
+            xstream.processAnnotations(GroupData.class);
+            List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+            return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
         }
-        XStream xstream = new XStream();
-        xstream.processAnnotations(GroupData.class);
-        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
-        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
-
     }
 
-    /*@DataProvider
-    public Iterator<Object[]> validGroups() {
-        List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[] { new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-        list.add(new Object[] { new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
-        list.add(new Object[] { new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
-        return list.iterator();
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromJSON() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")))) {
+            String json = "";
+            String line = reader.readLine();
+            while (null != line) {
+                json += line;
+                line = reader.readLine();
+            }
+            Gson gson = new Gson();
+            List<GroupData> groups =  gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType()); // List<GroupData>.class
+            return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+        }
     }
-*/
-    @Test(dataProvider = "validGroups")
+
+    @Test(dataProvider = "validGroupsFromJSON")
     public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
